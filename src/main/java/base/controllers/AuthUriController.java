622f584384529cf70ac2fc17f2727462638dea6e
Krishna <iamkrishna.krishna@gmail.com>
2022-01-28 18:53:19 +0530

    Added local checks 2 - works in local

--- src/main/java/base/controllers/AuthUriController.java
+++ src/main/java/base/controllers/AuthUriController.java
@@ -17,9 +17,9 @@ public class AuthUriController {
     @GetMapping("/auth")
     public String GetLoginURI(){
         System.out.println("Function: Auth");
-//        String redirectURL = CheckLocal.checkIsLocal() ? "http://localhost:8080/login" : "http://3.65.240.94:8080/login";
+        String redirectURL = CheckLocal.checkIsLocal() ? "http://localhost:8080/login" : "http://3.65.240.94:8080/login";
 
-        AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri().scope("user-library-read,user-read-email").build();
+        AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri().redirect_uri(URI.create(redirectURL)).scope("user-library-read,user-read-email").build();
         URI uri = authorizationCodeUriRequest.execute();
         System.out.println(uri);
         return uri.toString();
