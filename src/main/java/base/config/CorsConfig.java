90bd4ee2e4d8ecc79a20acfd5f5e417a525ca38f
Krishna <iamkrishna.krishna@gmail.com>
2022-03-05 18:07:53 +0530

    Updates CORS origins

--- src/main/java/base/config/CorsConfig.java
+++ src/main/java/base/config/CorsConfig.java
@@ -11,9 +11,13 @@ public class CorsConfig implements WebMvcConfigurer {
 
      @Override
         public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**").allowedOrigins("http://3.65.240.94:3000")
-                    .allowedOrigins("http://localhost:3000").allowedHeaders("*").allowCredentials(true);
-
-
+            registry.addMapping("/**")
+                    .allowedOrigins("http://3.65.240.94:3000")
+                    .allowedOrigins("http://3.65.240.94:8080")
+                    .allowedOrigins("http://3.65.240.94")
+                    .allowedOrigins("http://krishnaj.me")
+                    .allowedOrigins("http://krishnaj.me:8080")
+                    .allowedOrigins("http://localhost:3000")
+                 .allowedHeaders("*").allowCredentials(true);
         }
     }
